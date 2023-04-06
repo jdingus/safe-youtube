27975ee4cc651c11d2a223f7ff8de28f40c25fc6
mfgservicesgroup@gmail.com <josh.dingus@gmail.com>
2023-04-05 22:15:54 -0400

    updates to correct build

--- api/youtube-api.js
+++ api/youtube-api.js
@@ -1,19 +1,21 @@
-const axios = require("axios");
+const fetch = require("node-fetch");
 
 module.exports = async (req, res) => {
-  if (req.method !== "GET") {
-    res.status(405).send("Method Not Allowed");
-    return;
-  }
-
   const { channelId, searchQuery, maxResults } = req.query;
   const apiKey = process.env.YOUTUBE_API_KEY;
+
   const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${maxResults}${searchQuery ? `&q=${encodeURIComponent(searchQuery)}` : ''}`;
 
   try {
-    const response = await axios.get(url);
-    res.status(200).json(response.data);
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (response.status === 403) {
+      throw new Error("API key quota exceeded or invalid. Please check your YouTube API key.");
+    }
+
+    res.status(200).json(data);
   } catch (error) {
-    res.status(error.response.status).json({ message: error.message });
+    res.status(500).json({ error: error.message });
   }
 };
