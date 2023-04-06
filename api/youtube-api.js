319f853642f9b4dc64655b5e3535a014bde9d90e
mfgservicesgroup@gmail.com <josh.dingus@gmail.com>
2023-04-06 06:46:57 -0400

    updates again!

--- api/youtube-api.js
+++ api/youtube-api.js
@@ -1,4 +1,6 @@
-import fetch from "node-fetch";
+async function fetchModule() {
+  return import('node-fetch');
+}
 
 module.exports = async (req, res) => {
   const { channelId, searchQuery, maxResults } = req.query;
@@ -7,6 +9,7 @@ module.exports = async (req, res) => {
   const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${maxResults}${searchQuery ? `&q=${encodeURIComponent(searchQuery)}` : ''}`;
 
   try {
+    const fetch = (await fetchModule()).default;
     const response = await fetch(url);
     const data = await response.json();
 
@@ -14,8 +17,8 @@ module.exports = async (req, res) => {
       throw new Error("API key quota exceeded or invalid. Please check your YouTube API key.");
     }
 
-    res.setHeader("Access-Control-Allow-Origin", "*"); // Add this line
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); // Add this line
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
