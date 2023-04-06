1442addbd9161e2c090fcedba4010f8025c98bdc
mfgservicesgroup@gmail.com <josh.dingus@gmail.com>
2023-04-05 21:39:22 -0400

    api folder

---
+++ api/youtube-api.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+
+module.exports = async (req, res) => {
+  if (req.method !== "GET") {
+    res.status(405).send("Method Not Allowed");
+    return;
+  }
+
+  const { channelId, searchQuery, maxResults } = req.query;
+  const apiKey = process.env.YOUTUBE_API_KEY;
+  const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${maxResults}${searchQuery ? `&q=${encodeURIComponent(searchQuery)}` : ''}`;
+
+  try {
+    const response = await axios.get(url);
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(error.response.status).json({ message: error.message });
+  }
+};
