1762a86f8bcf35a73530958ce02d22d3bffb9866
mfgservicesgroup@gmail.com <josh.dingus@gmail.com>
2023-07-30 09:09:10 -0400

    aider: Removed logging of raw response text in `app.js`.
    # Context:
    USER: Debug Information:
    {
      "error": "Failed to execute 'json' on 'Response': body stream already read"
    }
    ASSISTANT: The error message "Failed to execute 'json' on 'Response': body stream already read" typically occurs when you try to read the body of a fetch response more than once. In your `app.js` file, you're first converting the response to text and logging it, then trying to convert it to JSON. This is not allowed because the response stream can only be read once.
    
    To fix this, you should remove the line where you're logging the raw response text. Here's the change:
    
    ```javascript
    app.js
    <<<<<<< ORIGINAL
        fetch(url)
            .then(response => {
                // Log the raw response text
                response.text().then(text => console.log(text));
                // Update debug info
                updateDebugInfo({ channelId, searchQuery, maxResults, url, responseStatus: response.status });
                if (response.status === 403) {
                    throw new Error("API key quota exceeded or invalid. Please check your YouTube API key.");
                }
                return response.json();
            })
    =======
        fetch(url)
            .then(response => {
                // Update debug info
                updateDebugInfo({ channelId, searchQuery, maxResults, url, responseStatus: response.status });
                if (response.status === 403) {
                    throw new Error("API key quota exceeded or invalid. Please check your YouTube API key.");
                }
                return response.json();
            })
    >>>>>>> UPDATED
    ```
    
    This change should resolve the error you're seeing.

--- app.js
+++ app.js
@@ -25,8 +25,6 @@ function getChannelVideos(channelId, searchQuery, maxResults) {
 
     fetch(url)
         .then(response => {
-            // Log the raw response text
-            response.text().then(text => console.log(text));
             // Update debug info
             updateDebugInfo({ channelId, searchQuery, maxResults, url, responseStatus: response.status });
             if (response.status === 403) {
